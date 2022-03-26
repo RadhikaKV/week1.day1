29f8a1d31c14853dd808ab4e0454040b8ec0f266
RadhikaKV <101965156+RadhikaKV@users.noreply.github.com>
2022-03-26 10:53:09 +0530

    Add files via upload

---
+++ NegativeOrPositiveNumber.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class NegativeOrPositiveNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 40;
+		if(number >= 0)
+			System.out.println("The given number is positive number");
+		else 
+			System.out.println("The given number is negative number");
+	}
+
+}
